92058b24e278a8c4804fe683a74611570beb1a98
Adarsh <adarshyadav65141@gmail.com>
2026-04-11 16:43:39 +0530

    fixed layoffs table

--- db_connect.py
+++ db_connect.py
@@ -7,8 +7,17 @@ load_dotenv()
 conn=sqlite3.connect("data.db",
 childProcessError=False)
 cursor=conn.cursor()
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE,password TEXT)
-""")
-conn.commit()
+cursor.execute("SELECT COUNT(*) FROM layoffs")
+count = cursor.fetchone()[0]
+
+if count == 0:
+    cursor.executemany("""
+    INSERT INTO layoffs (company, location, industry, total_laid_off, year, ai_adopted)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """, [
+        ("Google", "USA", "Tech", 12000, 2023, "Yes"),
+        ("Amazon", "USA", "E-commerce", 18000, 2023, "Yes"),
+        ("Meta", "USA", "Tech", 11000, 2022, "Yes"),
+        ("StartupX", "India", "Tech", 500, 2024, "No")
+    ])
+    conn.commit()
